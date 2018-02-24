18f3267b98c887ee29c14135f3c2c1e1b560de64
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-23 21:46:26 -0500

    completed set and bump

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Bundle } from './models/bundle';
+import { BundleService } from './services/bundle.service';
 
 @Component({
   selector: 'app-root',
@@ -12,21 +13,67 @@ export class AppComponent implements OnInit {
   bumpBuild: Bundle = new Bundle();
   setBuild: Bundle = new Bundle();
 
-  constructor () { }
+  lookup_error: string = null;
+  lookup_success: boolean = null;
+  bump_error: string = null;
+  bump_success: boolean = null;
+  set_error: string = null;
+  set_success: string = null;
+
+  constructor (private bundleService: BundleService) { }
 
   ngOnInit() {
     console.log('loadded');
   }
 
   getBuildNumber(lookupBuild: Bundle) {
-    console.log(lookupBuild);
+    // console.log(lookupBuild);
+    this.bundleService.read(lookupBuild.name)
+      .subscribe(
+        bundle => {
+          this.lookup_error = null;
+          this.lookup_success = true;
+          this.lookupBuild = bundle;
+        },
+        error => {
+          // console.log(<any>error)
+          this.lookup_error = <any>error;
+          this.lookup_success = null;
+        }
+      );
   }
 
   bumpBuildNumber(bumpBuild: Bundle) {
-    console.log(bumpBuild);
+    // console.log(bumpBuild);
+    this.bundleService.bump(bumpBuild.name)
+      .subscribe(
+        bundle => {
+          this.bump_error = null;
+          this.bump_success = true;
+          this.bumpBuild = bundle;
+        },
+        error => {
+          // console.log(<any>error)
+          this.bump_error = <any>error;
+          this.bump_success = null;
+        }
+      );
   }
 
   setBuildNumber(setBuild: Bundle) {
-    console.log(setBuild);
+    // console.log(setBuild);
+    this.bundleService.set(setBuild.name, setBuild.build_number)
+      .subscribe(
+        result => {
+          this.set_error = null;
+          this.set_success = result;
+          console.log(JSON.stringify(result))
+        },
+        error => {
+          // console.log(<any>error)
+          this.set_error = <any>error;
+          this.set_success = null;
+        }
+      );
   }
 }
