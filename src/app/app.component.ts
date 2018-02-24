717fdc4b9cafa79b372ada0cc0f79c9f0649846f
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-02-23 20:46:42 -0500

    added basic js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Bundle } from './models/bundle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  lookupBuild: Bundle = new Bundle();
+  bumpBuild: Bundle = new Bundle();
+  setBuild: Bundle = new Bundle();
+
+  constructor () { }
+
+  ngOnInit() {
+    console.log('loadded');
+  }
+
+  getBuildNumber(lookupBuild: Bundle) {
+    console.log(lookupBuild);
+  }
+
+  bumpBuildNumber(bumpBuild: Bundle) {
+    console.log(bumpBuild);
+  }
+
+  setBuildNumber(setBuild: Bundle) {
+    console.log(setBuild);
+  }
 }
